8132eb5680637d499e61adea22f01dc007737c27
Max Cai <maxster@xz.ax>
2024-01-10 14:10:15 -0800

    example, was able to print availible functions

--- get_prices.py
+++ get_prices.py
@@ -3,7 +3,25 @@ from web3 import Web3
 # Connect to base.org eth protocol
 w3 = Web3(Web3.HTTPProvider("https://mainnet.base.org"))
 
-# demo code? idk
-print(
-    w3.eth.get_block("latest")
-)
+# Get the latest block number
+# latest = w3.eth.block_number
+# print("Latest block number:", latest)
+
+# Get the latest block
+# block = w3.eth.get_block(latest)
+# print("Latest block:", block)
+
+import os
+import json
+
+contract_address = "0x2626664c2603336E57B271c5C0b26F421741e481"
+filename = f"{contract_address}.json"
+path = os.path.join(".", "abi", filename)
+
+with open(path, "r") as file:
+    abi = json.load(file)
+
+contract = w3.eth.contract(address=contract_address, abi=abi)
+
+for function_name in contract.functions:
+    print(function_name)
