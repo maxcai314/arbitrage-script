3f744caef6dfae4451531f641134b0555442dc96
Max Cai <maxster@xz.ax>
2024-01-10 16:15:20 -0800

    blorg

--- get_prices.py
+++ get_prices.py
@@ -27,14 +27,44 @@ contract = w3.eth.contract(address=contract_address, abi=abi)
 # for function_name in contract.functions:
     # print(function_name)
 
+token_abi = [
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [{"name": "", "type": "uint8"}],
+        "type": "function",
+    }
+]
+
+usdbc_address = Web3.to_checksum_address("0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca")
+usdbc_contract = w3.eth.contract(address=usdbc_address, abi=token_abi)
+usdbc_decimals = usdbc_contract.functions.decimals().call()
+print(f"usdbc uses {usdbc_decimals} decimals")
+
+weth_address = Web3.to_checksum_address("0x4200000000000000000000000000000000000006")
+weth_contract = w3.eth.contract(address=weth_address, abi=token_abi)
+weth_decimals = weth_contract.functions.decimals().call()
+print(f"weth uses {weth_decimals} decimals")
+
+input_amount = 1.0 # eth
+input_value = int(input_amount * 10 ** weth_decimals) # convert to smallest decimal unit
+
 quoter = contract.functions.quoteExactInputSingle
 params = (
-    Web3.to_checksum_address("0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca"), # tokenIn
-    Web3.to_checksum_address("0x0000000000000000000000000000000000000000"), # tokenOut
-    100, # amountIn
+    weth_address, # tokenIn
+    usdbc_address, # tokenOut
+    input_value, # amountIn
     500, # fee
     0, # sqrtPriceLimitX96
 )
 
+print(f"Finding the value of {input_amount} eth in usdbc on uniswap")
+
 result = quoter(params).call()
-print(result)
+print(f"""
+Amount out: {result[0] / 10 ** usdbc_decimals} usdbc
+Sqrt 96 price after: {result[1]}
+Initialized ticks crossed: {result[2]}
+gasEstimate: {result[3]}
+""")
