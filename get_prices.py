f1df8dc3e904cb81d48dc44847ac6954fc832d30
Max Cai <maxster@xz.ax>
2024-01-11 21:51:34 -0800

    uniswap only supports 6 of the tokens i found

--- get_prices.py
+++ get_prices.py
@@ -87,6 +87,18 @@ tokens = {
     weth: 0.004, # roughly 10 dollars
     usdbc: 10., # roughly 10 dollars
     tokenFor("0x50c5725949a6f0c72e6c4a641f24049a917db0cb"): 10., # DAI
+    # tokenFor("0x4158734d47fc9692176b5085e0f52ee0da5d47f1"): 2.5, # BAL
+    tokenFor("0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22"): 0.004, # cbETH
+    # tokenFor("0xAC1Bd2486aAf3B5C0fc3Fd868558b082a531B2B4"): 75000., # TOSHI
+    # tokenFor("0x4621b7A9c75199271F773Ebd9A499dbd165c3191"): 10., # DOLA
+    # tokenFor("0xEB466342C4d449BC9f53A865D5Cb90586f405215"): 10., # axlUSDC
+    # tokenFor("0xB79DD08EA68A908A97220C76d19A6aA9cBDE4376"): 10., # USD+
+    # tokenFor("0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452"): 0.004, # wstETH
+    # tokenFor("0xF6e932Ca12afa26665dC4dDE7e27be02A7c02e50"): 900000., # MOCHI
+    # tokenFor("0xd386a121991E51Eab5e3433Bf5B1cF4C8884b47a"): 160., # BVM
+    tokenFor("0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b"): 0.0002, # tBTC
+    # tokenFor("0x3055913c90Fcc1A6CE9a358911721eEb942013A1"): 3.5, # CAKE
+    tokenFor("0x27D2DECb4bFC9C76F0309b8E88dec3a601Fe25a8"): 600., # BALD
 }
 
 assert weth in tokens # pedantic, we need weth as a starting node
@@ -98,8 +110,12 @@ for start_token in tokens:
         if start_token == end_token:
             continue
         graph.add_token(end_token)
-        exchange_rate = exchangeRateOf(start_token, end_token, amount_in=tokens[start_token])
-        graph.add_exchange(Exchange("Uniswap", start_token, end_token, exchange_rate))
+        try:
+            exchange_rate = exchangeRateOf(start_token, end_token, amount_in=tokens[start_token])
+            graph.add_exchange(Exchange("Uniswap", start_token, end_token, exchange_rate))
+        except Exception as e:
+            print(f"Error getting exchange rate for {start_token} to {end_token}: {e}")
+            continue
 
 print("Graph created:")
 print(graph)
